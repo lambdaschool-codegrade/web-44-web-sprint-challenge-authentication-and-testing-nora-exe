569e85f1fec948dd77333f3eac108225d3041571
Nora C <nocorser@gmail.com>
2021-08-20 15:12:46 -0500

    restrict

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,30 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+/*IMPLEMENT
+  1- On valid token in the Authorization header, call next.
 
-    1- On valid token in the Authorization header, call next.
+  2- On missing token in the Authorization header,
+    the response body should include a string exactly as follows: "token required".
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
+  3- On invalid or expired token in the Authorization header,
+    the response body should include a string exactly as follows: "token invalid".
+*/
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
-};
+// const restricted = (req, res, next) => {
+//   const token = req.headers.authorization
+//   if (!token) {
+//     return next({ status: 401, message: 'token required' })
+//   }
+//   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+//     if (err) {
+//       next({ status: 401, message: 'Token invalid' })
+//     } else {
+//       req.decodedToken = decodedToken
+//       next()
+//     }
+//   })
+// }
+
+// module.exports = {
+//   restricted,
+// };
+
+module.exports = (req, res, next) => { next() }
