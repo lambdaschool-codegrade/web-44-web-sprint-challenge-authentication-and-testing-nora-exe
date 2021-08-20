174764dbb4d017bbe5415211dc519a8a3a87390e
Nora C <nocorser@gmail.com>
2021-08-20 15:37:20 -0500

    Tests WIP

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,20 @@
 // Write your tests here
+// 2 tests per API endpoint
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+/** Register
+ * Success
+ * Fail
+*/
+
+/** Login
+ * Success
+ * Fail
+*/
+
+/** Jokes
+ * Success
+ * Missing token
+*/
